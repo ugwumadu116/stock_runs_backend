66480b824ad5b87571289ea8583015879856014b
ugwumadu116 <ugwumadu116@gmail.com>
2019-11-23 00:08:13 +0100

    mbvjkvh

--- stockruns/stockruns/wsgi.py
+++ stockruns/stockruns/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockruns.stockruns.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockruns.settings')
 
 application = get_wsgi_application()
