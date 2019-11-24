dd900dcd885c404bb03ade56dd0e21e4326eb34a
ugwumadu116 <ugwumadu116@gmail.com>
2019-11-25 00:30:33 +0100

    added test case and travis file

--- stocks/tests.py
+++ stocks/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import json
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+class PhoneStorePost(APITestCase):
+    def test_create_phone(self):
+        self.assertEqual("test", "test")
