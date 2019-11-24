91df15e550b412b7d9a12298c7817039c33bba73
ugwumadu116 <ugwumadu116@gmail.com>
2019-11-24 22:30:54 +0100

    vdljfnljfskjd

--- stockruns/settings.py
+++ stockruns/settings.py
@@ -82,25 +82,25 @@ import dj_database_url
 
 
 
-# DATABASES = {
-#     'default': {
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': '',
+        'PORT': 5432,
+        'PASSWORD': 'juninho1',
+    }
+}
+# DATABASES = {}
+# if DEBUG == True:
+#     DATABASES = {
+#         'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'postgres',
 #         'USER': 'postgres',
 #         'HOST': 'db',
 #         'PORT': 5432,
-#     }
-# }
-
-# if DEBUG == True:
-#     DATABASES = {
-#         'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': "",
-#         'PORT': '5432',
 #         }
 #     }
 # else:
@@ -110,7 +110,7 @@ import django_heroku
 
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
